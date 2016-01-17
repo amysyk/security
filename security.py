cbffded505caf62b40c65050d45be7c185b7c473
amysyk <andriy.mysyk@gmail.com>
2016-01-17 15:08:39 -0800

    initial commit

---
+++ security.py
@@ -0,0 +1,20 @@
+import time
+import RPi.GPIO as io
+from flask import request
+from flask import Flask
+from config import *
+
+app = Flask(__name__)
+
+io.setmode(io.BCM)
+io.setup(IN_GARAGE_DOOR_PORT, io.IN, pull_up_down=io.PUD_UP)
+
+@app.route("/v1/Security/Doors/InGarageDoor/State")
+def inGarageDoorState():
+   if io.input(IN_GARAGE_DOOR_PORT):
+      state = "Open"
+   else:
+      state = "Closed"
+   return state
+if __name__ == "__main__":
+   app.run(host = '0.0.0.0', port = WEB_SERVER_PORT)
